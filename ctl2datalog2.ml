d06fb334cf53f3dc00f68b4039979f5dea647e1e
Martin Mirchev <mmirchev@comp.nus.edu.sg>
2023-09-15 12:42:49 +0800

    Small change to transflow

--- ctl2datalog2.ml
+++ ctl2datalog2.ml
@@ -306,7 +306,7 @@ let translation (ctl:ctl) : datalog =
       ] in
     let defaultRules = [ 
       ("transFlow", [VAR "x"; VAR "y"] ), [ Pos ("flow", [VAR "x"; VAR "y"]) ] ;
-      ("transFlow", [VAR "x"; VAR "z"] ), [ Pos ("flow", [VAR "x"; VAR "y"]); Pos ("flow", [VAR "y"; VAR "z"]) ] 
+      ("transFlow", [VAR "x"; VAR "z"] ), [ Pos ("flow", [VAR "x"; VAR "y"]); Pos ("transflow", [VAR "y"; VAR "z"]) ] 
       ] in
       (defaultDecs @ List.rev decs, defaultRules @ List.rev rules)
 
